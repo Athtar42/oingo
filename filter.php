78578274690c1c88f0355d8753af99815d5f3319
Xiaoran Sun <10081308+small-reynard@users.noreply.github.com>
2018-12-09 20:38:31 -0500

    delete login

--- filter.php
+++ filter.php
@@ -1,3 +1,72 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+		<title>Oingo | Filters</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<style>
+			.col-center-block {
+				float: none;
+				display: block;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			
+			.jumbotron {
+				background-image: url("img/primary-search-bg.jpg");
+				background-size: cover;
+			}
+		</style>
+	</head>
+
+	<body>
+		
+		<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+
+			<a class="navbar-brand" href="index.html">OINGO</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    				<span class="navbar-toggler-icon"></span>
+  				</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="index.html">OINGO</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Friends
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="friends.php">Friends List</a>
+							<a class="dropdown-item" href="#">Request Sent</a>
+							<a class="dropdown-item" href="#">Request Received</a>
+						</div>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="filter.php">Filter<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="test.php">Test</a>
+					</li>
+				</ul>
+			</div>
+
+		</nav>
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<span class="text-light"><h1 class="display-4">OINGO</h1></span>
+			</div>
+		</div>
+		
+		
+		
+		
+		<script src="js/jquery-3.3.1.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+
+</html>
