fd26955e9977c6d4fdf0d700552f69c0333d3883
suym7962018 <43250071+suym7962018@users.noreply.github.com>
2018-12-12 18:23:35 -0500

    post & comment

---
+++ comment.php
@@ -0,0 +1,21 @@
+<?php
+//make a comment
+
+session_start();
+$server="localhost";
+$db_username="root";
+$db_password="";
+$db_name="proj1"; 
+$con=mysqli_connect($server, $db_username, $db_password, $db_name);
+if(!$con)
+{
+	die("can't connect".mysqli_error());
+}
+$userid=$_SESSION['userid'];
+$noteid=$_POST['noteid'];
+$text=$_POST['text'];
+//add a comment
+$sql="insert into comment (userID, noteID, cText) values ('$userid', '$noteid', '$text')";
+$result=mysqli_query($con, $sql);
+
+?>
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
     $server="localhost";
     $db_username="root";
     $db_password="";
-    $db_name="proj1"; //数据库名字
+    $db_name="proj1"; 
 		$con=mysqli_connect($server, $db_username, $db_password, $db_name);
     if(!$con)
     {
@@ -21,30 +21,30 @@ function userdata($getemail)
 
 	return $user;
 }
-
+/*
 function add_user($email, $username, $password, $gender, $birthdate, $region)
 {
 	global $con;
-	$sql1="insert into user values ('', '$email', '$username', '$password', '$gender', '$birthdate', '$region')";
+	$sql1="insert into user(email, username, password, gender, birthdate, region) values ('$email', '$username', '$password', '$gender', '$birthdate', '$region')";
 	//userID改成自动增加的
 	$result1=mysqli_query($con, $sql1);
-	$user=mysqli_fetch_array($result1);
+	$user=userdata($email);
 	//add state
-	$sql2="insert into state values ('$user['userID']', 'default')";
-	$result2=mysqli_query($con, $sql2);
+	//$sql2="insert into state values ('$user['userID']', 'default')";
+	//$result2=mysqli_query($con, $sql2);
 	//add current
 	//$sql="insert into current values ('$user['userID']', 'default')";
 	//$result=mysqli_query($con, $sql);
 	
 	//return $user;
 }
-
+*/
 //list the user's friends
 function friendlist($userid)
 {
 	global $con;
 	$userid=$_POST['userid']; //获取当前用户的ID
-	$sql="select friendship.userID2, user.fbsql_username
+	$sql="select friendship.userID2, user.userName
 	      from friendship join user on friendship.userID2=user.userID
 	      where friendship.userID2='$userid'";
 	$result=mysqli_query($con, $sql);
@@ -52,48 +52,55 @@ function friendlist($userid)
 	
 	return $friend;
 }
-
+/*
 function add_note($userid, $notetext, $radius, $nrestrict, $nsID, $ifcomment)
 {
 	global $con;
 	$sql="insert into 'note' values ('', '$userid', '$notetext', now(), '$radius', '$nrestrict', '$nsID', '$ifcomment')";
-	$result=mysql_query($sql);
+	$result=mysql_query($con, $sql);
 }
 
 function add_tag($noteid, $tag)
 {
 	global $con;
 	$sql="insert into 'tag' values ('$noteid', '$tag')";
-	$result=mysql_query($sql);
+	$result=mysql_query($con, $sql);
 }
 
 function add_schedule()
 {
 	global $con;
 	$sql="insert into 'schedule' values ('', '$startTime', '$endTime', '$startDate', '$Weekday', '$endDate', '$Repetition')";
-	$result=mysql_query($sql);
+	$result=mysql_query($con, $sql);
 }
-
+*/
 function check_username($username)
 {
   global $con;
-	$sql="select count(*) from user where userName=$username";
-	$result=mysqli_query($sql);
+	$sql="select count(*) as num from user where userName='$username'";
+	$result=mysqli_query($con, $sql);
 	$count=mysqli_fetch_array($result);
-	if($count!==0)
+	if($count['num']!=0)
 	{
 		//echo "The userName has been occupied.";
 		echo"<script type='text/javascript'>alert('The userName has been occupied.');location='signup.html';</script>";
 	} 
 }
 
-function update_state($userid, $state)
+function statedata($userid)
 {
 	global $con;
-	$sql="update";
-	$result=mysqli_query($sql);
-	$new=mysqli_fetch_array($result);
-	$newstate=$new['state'];
+	$sql="select * from state where userID=$userid";
+	$result=mysqli_query($con, $sql);
+	$state=mysqli_fetch_array($result);
+	
+	return $state;
+}
+
+function dividetag($tag)
+{
+	$tags=explode('#', $tag);
+	return $tags;
 }
 
 ?>
