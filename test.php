bfd26f8f28810248f4de0f42fc94825ba5fc5cb0
Xiaoran Sun <10081308+small-reynard@users.noreply.github.com>
2018-12-12 18:25:34 -0500

    New Index

--- test.php
+++ test.php
@@ -1,6 +1,4 @@
-<?php
 
-?>
 <!DOCTYPE html>
 <html>
 
@@ -61,20 +59,52 @@
 				<span class="text-light"><h1 class="display-4">OINGO</h1></span>
 			</div>
 		</div>
+		<?php
+
+if ((isset($_POST["currentlocation"]))&&(isset($_POST["gapi"]))){
+	$location=$_POST['currentlocation'];
+	$gapi=$_POST['gapi'];	
+	$url = "http://maps.googleapis.com/maps/api/geocode/json?address=".$location."&key=".$gapi;
+	echo $url;
+	//$data = file_get_contents($url);
+	//$map = json_decode($data, true);
+	
+	$context = stream_context_create(array(
+	'http' => array(
+    'ignore_errors'=>true,
+    'method'=>'GET'
+     // for more options check http://www.php.net/manual/en/context.http.php
+    )
+	));
+	$response = json_decode(file_get_contents($url, false, $context));
+	echo $response;
+	echo $response['result'][0]['geometry']['location']['lat'];
+	echo $response['result'][0]['geometry']['location']['lng'];
+}
+else{
+	//echo "<script type='text/javascript'>alert('Location or API can not be empty!')</script>";
+}
+
+;
+?>
 		<div class="container">
 			<div class="row myCenter">
-				<div class="col-12 col-md-6 col-center-block">
+				<div class="col-8 col-md-6 col-lg-4 col-center-block">
 					<div class="card">
-						<div class="card-body" style="width: 21rem;">
+						<div class="card-body">
 							<h5 class="card-title">Test</h5>
-							<form name="test" action="#" method="post" class="">
+							<form name="test" action="test.php" method="post" class="">
 								<div class="form-group">
 									<label for="currenttime">Setting Current Time</label> 
-									<input type="datetime" name="currenttime" class="form-control">
+									<input type="datetime-local" name="currenttime" class="form-control">
 								</div>
 								<div class="form-group">
 									<label for="currentlocation">Location</label> 
 									<input type="text" name="currentlocation" class="form-control">
+								</div>
+								<div class="form-group">
+									<label for="gapi">API</label> 
+									<input type="text" name="gapi" class="form-control">
 								</div>
 								<button type="submit" name="submit" class="btn btn-primary">Save</button>
 							</form>
