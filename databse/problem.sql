a014eeec0e88fa97366d6244fdfd6bdbf0e07704
suym7962018 <43250071+suym7962018@users.noreply.github.com>
2018-12-19 02:04:24 -0500

    Update problem.sql

--- databse/problem.sql
+++ databse/problem.sql
@@ -7,10 +7,13 @@ FROM user join note #on user.userID=note.userID
 WHERE  (date(current.cTime) BETWEEN schedule.startDate AND schedule.endDate) AND (time(current.cTime) BETWEEN schedule.startTime AND schedule.endTime) 
                 AND ((schedule.repetition="no" AND date(current.cTime) = schedule.startDate) OR schedule.repetition="daily" OR (schedule.repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (schedule.repetition="monthly" AND day(current.cTime) = day(schedule.startDate)))
                 #AND (SQRT(POW(current.cLatitude,2) + POW(current.cLongitude,2)) - SQRT(POW(note.nLatitude,2) + POW(note.nLongitude,2))) <= note.radius
-                AND DEGREES(ACOS( COS(RADIANS(current.cLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(current.cLongitude)) + SIN(RADIANS(current.cLatitude)) * SIN(RADIANS(note.nLatitude)) ))*3961 < note.radius
+                #AND DEGREES(ACOS( COS(RADIANS(current.cLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(current.cLongitude)) + SIN(RADIANS(current.cLatitude)) * SIN(RADIANS(note.nLatitude)) ))*3961 < note.radius
                 #AND (ACOS( COS(RADIANS(note.nLatitude)) * COS(RADIANS(current.cLatitude)) * COS(RADIANS(current.cLongitude) - RADIANS(note.nLongitude)) + SIN(RADIANS(note.nLatitude)) * SIN(RADIANS(current.cLatitude)) )*3961) < note.radius
+				AND 6378.138 * 2 * ASIN(SQRT(POW(SIN((current.cLatitude*3.14/180-note.nLatitude*3.14/180)/2),2)+COS(current.cLatitude*3.14/180)*COS(note.nLatitude*3.14/180)*POW(SIN((current.cLongitude*3.14/180-note.nLongitude*3.14/180)/2),2)))*0.621 < note.radius
 				AND (note.nRestrict="all" OR (note.nRestrict="self" AND note.userID=user.userID) OR (note.nRestrict="friends" AND user.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
 )
+
+6378.138 * 2 * ASIN(SQRT(POW(SIN((current.cLatitude*3.14/180-note.nLatitude*3.14/180)/2),2)+COS(current.cLatitude*3.14/180)*COS(note.nLatitude*3.14/180)*POW(SIN((current.cLongitude*3.14/180-note.nLongitude*3.14/180)/2),2)))*0.621
 
 CREATE VIEW tempFilter AS 
 (
@@ -20,7 +23,8 @@ FROM filter join note #on note.userID=filter.userID
 			join current on filter.userID=current.userID 
 WHERE  (date(current.cTime) BETWEEN schedule.startDate AND schedule.endDate) AND (time(current.cTime) BETWEEN schedule.startTime AND schedule.endTime) 
                 AND ((schedule.repetition="no" AND date(current.cTime) = schedule.startDate) OR schedule.repetition="daily" OR (schedule.repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (schedule.repetition="monthly" AND day(current.cTime) = day(schedule.startDate)))
-                AND (ACOS( COS(RADIANS(filter.fLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(filter.fLongitude)) + SIN(RADIANS(filter.fLatitude)) * SIN(RADIANS(note.nLatitude)) )*3961)< note.radius
+                AND 6378.138 * 2 * ASIN(SQRT(POW(SIN((filter.fLatitude*3.14/180-note.nLatitude*3.14/180)/2),2)+COS(filter.fLatitude*3.14/180)*COS(note.nLatitude*3.14/180)*POW(SIN((filter.fLongitude*3.14/180-note.nLongitude*3.14/180)/2),2)))*0.621 < note.radius
+				#AND (ACOS( COS(RADIANS(filter.fLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(filter.fLongitude)) + SIN(RADIANS(filter.fLatitude)) * SIN(RADIANS(note.nLatitude)) )*3961)< note.radius
 				AND (filter.fRestrict="all" OR (filter.fRestrict="self" AND filter.userID=note.userID) OR (filter.fRestrict="friends" AND filter.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
 				AND (filter.apply="1")
 )
