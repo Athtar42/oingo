e17b6415254ddae19c74492d921e7dc9fdfe2c41
Xiaoran Sun <10081308+small-reynard@users.noreply.github.com>
2018-12-18 23:23:20 -0500

    Update problem.sql

--- databse/problem.sql
+++ databse/problem.sql
@@ -1,14 +1,15 @@
 CREATE VIEW tempNote AS 
 (
 SELECT user.userID as filteruser, note.noteID, note.userID as noteuser
-FROM user join note on user.userID=note.userID
+FROM user join note #on user.userID=note.userID
 			join schedule on note.nsID=schedule.sID 
 			join current on user.userID=current.userID
 WHERE  (date(current.cTime) BETWEEN schedule.startDate AND schedule.endDate) AND (time(current.cTime) BETWEEN schedule.startTime AND schedule.endTime) 
-                AND ((repetition="no" AND date(current.cTime) = schedule.startDate) OR repetition="daily" OR (repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (repetition="monthly" AND day(current.cTime) = day(schedule.startDate)) OR (repetition="yearly" AND month(current.cTime) = month(schedule.startDate)))
+                AND ((schedule.repetition="no" AND date(current.cTime) = schedule.startDate) OR schedule.repetition="daily" OR (schedule.repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (schedule.repetition="monthly" AND day(current.cTime) = day(schedule.startDate)))
                 #AND (SQRT(POW(current.cLatitude,2) + POW(current.cLongitude,2)) - SQRT(POW(note.nLatitude,2) + POW(note.nLongitude,2))) <= note.radius
-                AND (ACOS( COS(RADIANS(current.cLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(current.cLongitude)) + SIN(RADIANS(current.cLatitude)) * SIN(RADIANS(note.nLatitude)) )*3961) < note.radius
-				AND (note.nRestrict="all" OR (note.nRestrict="self" AND note.userID=user.userID) OR (note.nRestrict="friend" AND user.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
+                AND DEGREES(ACOS( COS(RADIANS(current.cLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(current.cLongitude)) + SIN(RADIANS(current.cLatitude)) * SIN(RADIANS(note.nLatitude)) ))*3961 < note.radius
+                #AND (ACOS( COS(RADIANS(note.nLatitude)) * COS(RADIANS(current.cLatitude)) * COS(RADIANS(current.cLongitude) - RADIANS(note.nLongitude)) + SIN(RADIANS(note.nLatitude)) * SIN(RADIANS(current.cLatitude)) )*3961) < note.radius
+				AND (note.nRestrict="all" OR (note.nRestrict="self" AND note.userID=user.userID) OR (note.nRestrict="friends" AND user.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
 )
 
 CREATE VIEW tempFilter AS 
@@ -18,9 +19,9 @@ FROM filter join note #on note.userID=filter.userID
 			join schedule on filter.fsID=schedule.sID
 			join current on filter.userID=current.userID 
 WHERE  (date(current.cTime) BETWEEN schedule.startDate AND schedule.endDate) AND (time(current.cTime) BETWEEN schedule.startTime AND schedule.endTime) 
-                AND ((repetition="no" AND date(current.cTime) = schedule.startDate) OR repetition="daily" OR (repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (repetition="monthly" AND day(current.cTime) = day(schedule.startDate)) OR (repetition="yearly" AND month(current.cTime) = month(schedule.startDate)))
+                AND ((schedule.repetition="no" AND date(current.cTime) = schedule.startDate) OR schedule.repetition="daily" OR (schedule.repetition="weekly"  AND current.cWeekday = schedule.weekday) OR (schedule.repetition="monthly" AND day(current.cTime) = day(schedule.startDate)))
                 AND (ACOS( COS(RADIANS(filter.fLatitude)) * COS(RADIANS(note.nLatitude)) * COS(RADIANS(note.nLongitude) - RADIANS(filter.fLongitude)) + SIN(RADIANS(filter.fLatitude)) * SIN(RADIANS(note.nLatitude)) )*3961)< note.radius
-				AND (filter.fRestrict="all" OR (filter.fRestrict="self" AND filter.userID=note.userID) OR (filter.fRestrict="friend" AND filter.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
+				AND (filter.fRestrict="all" OR (filter.fRestrict="self" AND filter.userID=note.userID) OR (filter.fRestrict="friends" AND filter.userID in (SELECT userID2 FROM friendship WHERE userID1=note.userID)))
 				AND (filter.apply="1")
 )
 
